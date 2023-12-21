6fe468dfb421d4ae6e7ee25e00b2d15d7a014334
eyalor <eyaloren86@gmail.com>
2023-12-21 12:25:13 +0200

    fixing the log file

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'php/html_page_init.php';
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>רצי הבירה - יומן ריצה</title>
-    <link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.5.1/build/cssreset/cssreset-min.css">
+    <link rel="stylesheet" type="text/css" href="https://yui-s.yahooapis.com/3.5.1/build/cssreset/cssreset-min.css">
     <link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet"
           type="text/css"/>
     <link href='./css/fullcalendar.css?v=<?php echo CSS_VERSION;?>' rel='stylesheet' type='text/css'/>
